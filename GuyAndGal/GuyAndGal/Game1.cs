1e0bc4c4ade93c1e6d6178c6cc0976811b0fa261
Shakenbeet <68769498+Shakenbeet@users.noreply.github.com>
2021-01-03 12:02:09 -0600

    Add files via upload

---
+++ GuyAndGal/GuyAndGal/Game1.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using MonoGame.Extended.Screens;
+using MonoGame.Extended.Screens.Transitions;
+
+namespace GuyAndGal
+{
+    public class Game1 : Game
+    {
+        public GraphicsDeviceManager _graphics;
+        public SpriteBatch _spriteBatch;
+
+        private readonly ScreenManager _screenManager;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            _screenManager = new ScreenManager();
+            Components.Add(_screenManager);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+            _graphics.PreferredBackBufferWidth = 1920;
+            _graphics.PreferredBackBufferHeight = 1080;
+            _graphics.IsFullScreen = true;
+            _graphics.ApplyChanges();
+            
+            LoadMenu();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        private void LoadMenu()
+        {
+            _screenManager.LoadScreen(new Menu(this), new FadeTransition(GraphicsDevice, Color.Black));
+        }
+
+        public void LoadLevel()
+        {
+            MediaPlayer.Stop();
+            _screenManager.LoadScreen(new Playground(this), new FadeTransition(GraphicsDevice, Color.Black));
+        }
+    }
+}
---
+++ GuyAndGal/GuyAndGal/Menu.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using MonoGame.Extended.Screens;
+using MonoGame.Extended.Screens.Transitions;
+
+namespace GuyAndGal
+{
+    class Menu : GameScreen
+    {
+        private new Game1 Game => (Game1)base.Game;
+
+        private Texture2D menu;
+        private Song theme;
+
+        public Menu(Game1 game) : base(game) { }
+
+        public override void LoadContent()
+        {
+            base.LoadContent();
+            menu = Game.Content.Load<Texture2D>("menuLogo");
+            theme = Game.Content.Load<Song>("HatInTimeTheme");
+            MediaPlayer.Play(theme);
+            MediaPlayer.Volume = 0.1f;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            KeyboardState kstate = Keyboard.GetState();
+            if (kstate.IsKeyDown(Keys.Enter))
+                Game.LoadLevel();
+
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            Game.GraphicsDevice.Clear(Color.CornflowerBlue);
+            Game._spriteBatch.Begin();
+            Game._spriteBatch.Draw(menu, new Vector2(Game._graphics.PreferredBackBufferWidth / 2, Game._graphics.PreferredBackBufferHeight / 2), null, Color.White, 0f, new Vector2(menu.Width / 2, menu.Height / 2), Vector2.One, SpriteEffects.None, 0f);
+            Game._spriteBatch.End();
+        }
+    }
+}
